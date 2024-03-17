927e6197d5ef535e920be40f60782f587743011b
Harith Onigemo <onigemotosin@gmail.com>
2024-03-17 07:39:28 +0400

    :lipstick: Fixed contact us modal on homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
+import React from "react";
 import {
-    faFacebook,
-    faInstagram,
-    faTwitter,
-    faWhatsapp,
-  } from "@fortawesome/free-brands-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="footer bg-zinc-950 text-white w-full">
-        <div className="footer-container flex flex-col items-center justify-center w-[80vw] m-auto pt-20 pb-20">
-          <div className="footer-links w-full flex justify-evenly items-start">
-            <div className="footer-link">
-              {/* <h3 className="text-lg font-bold text-start pb-2 text-[#363636]">
+      <div className="footer-container flex flex-col items-center justify-center w-[80vw] m-auto pt-20 pb-20">
+        <div className="footer-links w-full flex justify-evenly items-start">
+          <div className="footer-link">
+            {/* <h3 className="text-lg font-bold text-start pb-2 text-[#363636]">
                 About Us
               </h3> */}
-              <ul className="text-sm flex flex-col items-start justify-center">
-                <li className="pb-2"><Link href='/admin'>
-                Admin Login
-                </Link></li>
-                <li className="pb-2">Company</li>
-                <li className="pb-2">About Us</li>
-                <li className="pb-2">Careers</li>
-                <li className="pb-2">Team</li>
-                <li className="pb-2">Contact Us</li>
-              </ul>
-            </div>
-            <div className="social-links flex flex-col items-start justify-center h-full">
+            <ul className="text-sm flex flex-col items-start justify-center">
+              <li className="pb-2">
+                <Link href="/admin">Admin Login</Link>
+              </li>
+              <li className="pb-2">
+                <Link href="/about">Company</Link>
+              </li>
+              <li className="pb-2">
+                <Link href="/about">About Us</Link>
+              </li>
+              <li className="pb-2">
+                <Link href="/about">Our Team</Link>
+              </li>
+              <li className="pb-2">
+                <Link href="/about">Contact</Link>
+              </li>
+            </ul>
+          </div>
+          {/* <div className="social-links flex flex-col items-start justify-center h-full">
               <h3 className="text-lg font-bold text-start pb-2">Follow us</h3>
               <ul className="text-sm flex gap-2 items-center h-full justify-center">
                 <li className="pb-2">
@@ -46,16 +52,16 @@ const Footer = () => {
                   <FontAwesomeIcon icon={faWhatsapp} className="text-white" />
                 </li>
               </ul>
-            </div>
-          </div>
+            </div> */}
         </div>
-        <div className="copy flex justify-start items-center pb-9">
-            <p className="text-xs m-auto text-start pb-2 w-[90vw]">
-                © 2023 Maash Engineering. All rights reserved.
-            </p>
-        </div>
-      </footer>
-  )
-}
+      </div>
+      <div className="copy flex justify-start items-center pb-9">
+        <p className="text-xs m-auto text-start pb-2 w-[90vw]">
+          © 2023 Maash Engineering. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,80 +1,139 @@
-'use client';
-import React, { useEffect, useState } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import Modal from './Modal';
+"use client";
+import React, { useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import AdminModal from "./admin/AdminModal";
+import { ModalHeader, ModalFooter, ModalBody, Button } from "@chakra-ui/react";
+import {
+  faClose,
+  faEnvelope,
+  faLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function HomeCarousel() {
+  const autoplayOptions = {
+    delay: 4000,
+    rootNode: (emblaRoot) => emblaRoot.parentElement,
+  };
 
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay(autoplayOptions),
+  ]);
+  const [showModal, setShowModal] = useState(false);
 
-    const autoplayOptions = {
-        delay: 4000,
-        rootNode: (emblaRoot) => emblaRoot.parentElement
-      }
-      
+  const services = [
+    {
+      title: "Oil and Gas",
+      bg_image: "oil_and_gas",
+      description:
+        "With a proven track record in integrated Oil & Gas operations and a strong commitment to make meaningful contributions to the economy of Nigeria, Maash Engineering is being increasingly accepted as a preferred strategic partner. \n \n We seek to maximize value creation and strengthen the integration of our operations all the way down the value chain.",
+    },
+    {
+      title: "Heavy Duty Leasing",
+      bg_image: "heavy_duty_leasing",
+      description:
+        "We lease Construction equipments ranging from bulldozers, airlifts, heavy duty cranes, air compressors, concrete pumps and mixers, Bulldozers (crawlers and wheels), Cranes (heavy duty mobile, tower and crane), dumpers, excavators, compressors, ton heavy duty loader, etc.",
+    },
+    {
+      title: "Pipeline Welding & Maintenance",
+      bg_image: "pipeline_welding",
+      description:
+        "We are well know and highly respected for our honesty and Integrity and posses many years of technical and commercial Experience in maintenances and repairs/upgrade of oil and gas pipeline.",
+    },
+  ];
 
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay(autoplayOptions)])
-    const [showModal, setShowModal] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(services[0]);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
+  const numberOfSlides = 3;
 
-
-    const services = [
-        {
-            title: 'Oil and Gas',
-            bg_image : 'oil_and_gas',
-            description: 'With a proven track record in integrated Oil & Gas operations and a strong commitment to make meaningful contributions to the economy of Nigeria, Maash Engineering is being increasingly accepted as a preferred strategic partner. \n \n We seek to maximize value creation and strengthen the integration of our operations all the way down the value chain.'
-        },
-        {
-            title: 'Heavy Duty Leasing',
-            bg_image : 'heavy_duty_leasing',
-            description: 'We lease Construction equipments ranging from bulldozers, airlifts, heavy duty cranes, air compressors, concrete pumps and mixers, Bulldozers (crawlers and wheels), Cranes (heavy duty mobile, tower and crane), dumpers, excavators, compressors, ton heavy duty loader, etc.'
-        },
-        {
-            title: 'Pipeline Welding & Maintenance',
-            bg_image : 'pipeline_welding',
-            description: 'We are well know and highly respected for our honesty and Integrity and posses many years of technical and commercial Experience in maintenances and repairs/upgrade of oil and gas pipeline.'
-        },
-];
-
-    const [currentSlide, setCurrentSlide] = useState(services[0]);
-    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-
-    const numberOfSlides = 3;
-
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setCurrentSlideIndex((currentSlideIndex + 1) % numberOfSlides);
-    //     }, 15000);
-    // }, [currentSlideIndex]);
+  // useEffect(() => {
+  //     setTimeout(() => {
+  //         setCurrentSlideIndex((currentSlideIndex + 1) % numberOfSlides);
+  //     }, 15000);
+  // }, [currentSlideIndex]);
 
   return (
     <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-        {
-            services.map((service, index) => (
-                <div key={service.title} className={`slider-items embla__slide ${service.bg_image}`}>
-                <div className="slider-content embla__slide__img w-full h-full  flex justify-center items-center">
-                    <div className="content-container w-11/12 opacity-100">
-                        <h1 className='text-4xl lg:text-7xl font-bold text-white'>{service.title}</h1>
-                        <p className='text-white lg:w-[465px] whitespace-pre-line pt-4'>{service.description}</p>
-                        <button onClick={() => setShowModal(true)} className='bg-white text-black font-bold py-2 px-14 rounded mt-4'>
-                            Contact Us
-                        </button>
-                    </div>
-                </div>
+      <div className="embla__container">
+        {services.map((service, index) => (
+          <div
+            key={service.title}
+            className={`slider-items embla__slide ${service.bg_image}`}
+          >
+            <div className="slider-content embla__slide__img w-full h-full  flex justify-center items-center">
+              <div className="content-container w-11/12 opacity-100">
+                <h1 className="text-4xl lg:text-7xl font-bold text-white">
+                  {service.title}
+                </h1>
+                <p className="text-white lg:w-[465px] whitespace-pre-line pt-4">
+                  {service.description}
+                </p>
+                <button
+                  onClick={() => setShowModal(true)}
+                  className="bg-white text-black font-bold py-2 px-14 rounded mt-4"
+                >
+                  Contact Us
+                </button>
+              </div>
             </div>
-            ))
-        }
-        </div>
-        {showModal && <Modal onClose={() => setShowModal(false)} />}
+          </div>
+        ))}
+      </div>
+      <AdminModal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <ModalHeader>Contact Us</ModalHeader>
+        <ModalBody>
+          <div className="modal-ch p-5 w-full">
+            <div className="modal-body h-full flex justify-center items-center">
+              <div className="modal-body-contents w-full h-full flex justify-center gap-6 items-center">
+                <div className="email p-2 lg:p-4 flex justify-center items-center gap-2 rounded-lg bg-neutral-800 text-white">
+                  <a
+                    href="mailto:info@maashengineering.com"
+                    className="text-4xl"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </a>
+                  <p className="cursor-pointer">Email us</p>
+                </div>
 
+                <div className="phone p-2 lg:p-4 flex justify-center items-center gap-2 rounded-lg bg-rose-500 text-white">
+                  <a
+                    href="tel:+234(0) 333 53851"
+                    className="text-4xl"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={faPhone} />
+                  </a>
+                  <p className="cursor-pointer">Call Us</p>
+                </div>
+
+                <div className="address p-2 lg:p-4 flex justify-center items-center gap-2 rounded-lg bg-sky-500 text-white">
+                  <a
+                    href="https://maps.app.goo.gl/oXBhZzcEfo73RwUQA"
+                    className="text-4xl"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={faLocationDot} />
+                  </a>
+                  <p className="cursor-pointer">Find Us</p>
+                </div>
+              </div>
+            </div>
+            {/* <div className='modal-footer'>
+                <button>Cancel</button>
+                <button>Save</button>
+            </div> */}
+          </div>
+        </ModalBody>
+      </AdminModal>
     </div>
-  )
+  );
 }
 
 export default HomeCarousel;
-
 
 // div className="w-full h-96 relative">
 //         <Image
